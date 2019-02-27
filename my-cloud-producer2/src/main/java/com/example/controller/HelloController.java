f2d95bf1f7d36636bac327e6844e6db9410c609c
mmyuan <yuanke52014@sohu.com>
2019-02-27 16:31:16 +0800

    添加注释

--- my-cloud-producer2/src/main/java/com/example/controller/HelloController.java
+++ my-cloud-producer2/src/main/java/com/example/controller/HelloController.java
@@ -1,14 +1,25 @@
 package com.example.controller;
 
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+	
 	@RequestMapping("/hello")
 	public String hello(@RequestParam String name) {
+		logger.info("my-cloud-producer2: --> request two name is " + name);
+/* 为了开启 zuul的retry功能		
+		try {
+			Thread.sleep(1000000);
+		} catch (Exception e) {
+			logger.error(" hello two error", e);
+		}
+*/
 		return "hello " + name + "，this is second messge, 这是服务提供者2，请刷新浏览器是否有自动负载均衡";
 	}
 }
